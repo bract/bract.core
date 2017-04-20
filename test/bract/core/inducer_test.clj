46ca98e70bfb42c4aa7655dd0f7a1fdb2b272c5d
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-04-20 16:23:33 +0530

    add inducer tests

--- test/bract/core/inducer_test.clj
+++ test/bract/core/inducer_test.clj
@@ -10,9 +10,34 @@
 (ns bract.core.inducer-test
   (:require
     [clojure.test :refer :all]
+    [bract.core.config  :as config]
     [bract.core.inducer :as inducer]))
 
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-export-as-sysprops
+  (is (nil? (System/getProperty "foo")))
+  (is (nil? (System/getProperty "bar")))
+  (inducer/export-as-sysprops {:bract.core/config {"bract.core.exports" ["foo"
+                                                                         "bar"]
+                                                   "foo" "foo10"
+                                                   "bar" "bar20"
+                                                   "baz" "baz30"}})
+  (is (= "foo10" (System/getProperty "foo")))
+  (is (= "bar20" (System/getProperty "bar"))))
+
+
+(def launcher-store (volatile! 0))
+
+
+(defn launcher-inc
+  [context]
+  (vswap! launcher-store #(inc ^long %)))
+
+
+(deftest test-invoke-launcher
+  (vreset! launcher-store 0)
+  (inducer/invoke-launcher {:bract.core/launch? false})
+  (is (zero? @launcher-store))
+  (inducer/invoke-launcher {:bract.core/launch? true
+                            :bract.core/config {"bract.core.launcher" "bract.core.inducer-test/launcher-inc"}})
+  (is (= 1 @launcher-store)))
