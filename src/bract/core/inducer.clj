e68e93ee97d7e558d8cdb14c19c506bdbf83240d
Shantanu Kumar <kumar.shantanu@gmail.com>
2018-01-06 23:18:46 +0530

    fixup! avoid unnecessary string concatenation

--- src/bract/core/inducer.clj
+++ src/bract/core/inducer.clj
@@ -170,7 +170,7 @@
   (if (kdef/ctx-launch? context)
     (let [launcher (-> (kdef/ctx-config context)
                      kdef/cfg-launcher)]
-      (echo/echo "Launcher name: " launcher)
+      (echo/echo "Launcher name:" launcher)
       (launcher context))
     (do
       (echo/echo "Launch not enabled, skipping launch.")
